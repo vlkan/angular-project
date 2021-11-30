859e259197d741f340bf83dbb807d229d86fff81
vlkan <thkvolkanonder@gmail.com>
2021-11-30 16:49:40 +0300

    fix jquery

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -34,4 +34,11 @@ export class CategoryComponent implements OnInit {
     else
       return "list-group-item"
   }
+
+  getAllCategoryClass(){
+    if(!this.currentCategory)
+      return "list-group-item active"
+    else
+      return "list-group-item"
+  }
 }
